b3c03d3eb7794a30e1f4bf5379ba5584c6d3b8ec
Nicolas <aponcenicolas@gmail.com>
2021-02-22 20:44:42 -0500

    sss

--- src/test/java/lux/pe/customer/ApiRestCustomerApplicationTests.java
+++ src/test/java/lux/pe/customer/ApiRestCustomerApplicationTests.java
@@ -1,13 +1,9 @@
 package lux.pe.customer;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiRestCustomerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
